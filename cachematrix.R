72e693851aa1efbaada016a792fd42dec11f1697
Emma Thompson <thom1097@flinders.edu.au>
2020-10-03 15:39:58 -0700

    My Solution for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## INITIALISING OBJECTS- (x = a matrix, iv(inverse) is set to be an empty 
+##                        object)
+## DEFINING OBJECTS- 1) set function (y) writes x into the parent environment 
+##                      and sets iv to null
+##                  2)get <- returns the matrix x
+##                  3) setmatrixiv sets the iv in the parent environment using 
+##                      dummy 
+##                    argument parentmatrixiv
+##                  4)getmatrixiv returns the matrix inverse
+## LIST- creates a list of the functions in makeCacheMatrix enabling them to be 
+## used by cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    iv <- NULL
+    set <- function(y) {
+             x <<- y
+            iv <<- NULL
+    }
+    get <- function()x
+    setmatrixiv <- function(parentmatrixiv) iv <<-parentmatrixiv
+    getmatrixiv <- function() iv
+    list(set = set, get = get, 
+         setmatrixiv = setmatrixiv, 
+         getmatrixiv = getmatrixiv)
 }
 
 
-## Write a short comment describing this function
+## Cachesolve function 
+##  1- uses getmatrix function to return the inverse from the parent environment
+##  2- uses an if loop to identify if the returned object is an inverse or NULL,
+##        if inverse it will display a message and eventually return an inverse 
+##        matrix
+##        if NULL the function accesses the matrix x, creates its inverse matrix 
+##        using solve(x) and returns the matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+          iv <- x$getmatrixiv()
+          if(!is.null(iv)){
+                  message("getting cached data")
+                  return(iv)
+          }
+          data <-x$get()
+          iv <-solve(data, ...)
+          x$setmatrixiv(iv)
+          iv
 }
